8c92c324f1423c320fe675c730b7d862271b3ed4
Johan11B <jbriseno@ucundinamarca.edu.co>
2025-03-04 13:59:14 -0500

    Atributos de biblioteca

--- src/main/java/org/example/Libro.java
+++ src/main/java/org/example/Libro.java
@@ -1,4 +1,16 @@
 package org.example;
 
 public class Libro {
+
+    private String titulo;
+    private String autor;
+    private int id;
+    private boolean disponible;
+
+    public Libro(String titulo, String autor, int id) {
+        this.titulo = titulo;
+        this.autor = autor;
+        this.id = id;
+        this.disponible = true;
+    }
 }

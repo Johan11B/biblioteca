4c25a4c007a90b9be5ebc915000dc18d45e8ab76
DanielEcm25 <destebancontreras@ucundinamarca.edu.co>
2025-03-04 14:23:14 -0500

    Menú

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,21 +1,50 @@
 package org.example;
-
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Scanner opcion = new Scanner(System.in);
+        int op = opcion.nextInt();
+        while (true) {
+            try {
+                System.out.println("Bienvenido a la Biblioteca JJD");
+                System.out.println("Por favor seleccione qué desea hacer:");
+                System.out.println("1. Agregar un libro");
+                System.out.println("2. Buscar un libro por título");
+                System.out.println("3. Buscar un libro por autor");
+                System.out.println("4. Eliminar un libro");
+                System.out.println("5. Visualizar todos los libros disponibles");
+                System.out.println("6. Salir del programa");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+                System.out.print("Ingrese una opción: ");
 
-            System.out.println("Daniel");
-            System.out.println("HOLA");
-
+                if (op >= 1 && op <= 6) {
+                    break;
+                } else {
+                    System.out.println("Por favor, seleccione una opción válida (1-6).");
+                }
+            } catch (NumberFormatException e) {
+                System.out.println("Error: Ingrese un número válido.");
+            }
+        }
+        switch (op) {
+            case 1:
+                System.out.println("Opción 1 seleccionada: Agregar un libro.");
+                break;
+            case 2:
+                System.out.println("Opción 2 seleccionada: Buscar un libro por título.");
+                break;
+            case 3:
+                System.out.println("Opción 3 seleccionada: Buscar un libro por autor.");
+                break;
+            case 4:
+                System.out.println("Opción 4 seleccionada: Eliminar un libro.");
+                break;
+            case 5:
+                System.out.println("Opción 5 seleccionada: Visualizar todos los libros disponibles.");
+                break;
+            case 6:
+                System.out.println("Saliendo del programa...");
+                break;
+        }
         }
     }
-}

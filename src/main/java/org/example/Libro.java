0699e76f4a07b2492b63b325afb820dafb902e2e
Johan11B <jbriseno@ucundinamarca.edu.co>
2025-03-04 14:27:32 -0500

    Estructura de la clase libro

--- src/main/java/org/example/Libro.java
+++ src/main/java/org/example/Libro.java
@@ -5,12 +5,59 @@ public class Libro {
     private String titulo;
     private String autor;
     private int id;
+    private int anioPublicacion;
     private boolean disponible;
+    private String genero;
 
-    public Libro(String titulo, String autor, int id) {
+    public Libro(String titulo, String autor, int id, int anioPublicacion, String genero) {
         this.titulo = titulo;
         this.autor = autor;
         this.id = id;
+        this.anioPublicacion = anioPublicacion;
         this.disponible = true;
+        this.genero = genero;
+    }
+
+    // Getters y Setters
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public int getAnioPublicacion() {
+        return anioPublicacion;
+    }
+
+    public void setAnioPublicacion(int anioPublicacion) {
+        this.anioPublicacion = anioPublicacion;
+    }
+
+    public boolean isDisponible() {
+        return disponible;
+    }
+
+    public void setDisponible(boolean disponible) {
+        this.disponible = disponible;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
     }
 }

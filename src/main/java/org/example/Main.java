420a3cb312367bf81f90c30ee5dab57d9644b1a9
DanielEcm25 <destebancontreras@ucundinamarca.edu.co>
2025-03-04 19:59:41 -0500

    Solucionando configuración musc.xml

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,10 @@
 package org.example;
-import java.util.Scanner;
+
+
 public class Main {
     public static void main(String[] args) {
-                //Llamando la clase biblioteca
-                Biblioteca.menu();
+        //Llamando la clase biblioteca
+        Biblioteca.menu();
 
-            }
+    }
 }

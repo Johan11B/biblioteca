406df6645b2715d01bc837e2ad2abc874e3e0582
DanielEcm25 <destebancontreras@ucundinamarca.edu.co>
2025-03-04 19:52:03 -0500

    Solucionando errores

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,9 @@
 package org.example;
-<<<<<<< HEAD
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
+                //Llamando la clase biblioteca
+                Biblioteca.menu();
 
-
-
-
-public class Main {
-    public static void main(String[] args) {
-        //Llamando la clase biblioteca
-
-        Biblioteca.menu();
-
-        }
-    }
+            }
+}
